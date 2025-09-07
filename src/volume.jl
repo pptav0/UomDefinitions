faf2bf5e84c1879a4c3e7e1b7661fab3409bdb9a
Jose Guzman <pepetavo@pm.me>
2025-09-07 17:03:57 +0100

    Added rounded digits number when converting

--- src/volume.jl
+++ src/volume.jl
@@ -26,10 +26,10 @@ mutable struct Volume{U<:VolumeUnit}
 end
 
 # ergonomic constructors
-Volume(v::Real, ::M3)  = Volume{M3}(float(v))
-Volume(v::Real, ::BBL) = Volume{BBL}(float(v))
-Volume(v::Real, ::L)   = Volume{L}(float(v))
-Volume(v::Real, ::SCF) = Volume{SCF}(float(v))
+Volume(v::Real, ::M3; 	digits::Int=4) 	= Volume{M3}(round(float(v); digits=digits))
+Volume(v::Real, ::BBL; 	digits::Int=4) 	= Volume{BBL}(round(float(v); digits=digits))
+Volume(v::Real, ::L; 	digits::Int=4) 	= Volume{L}(round(float(v); digits=digits))
+Volume(v::Real, ::SCF; 	digits::Int=4) 	= Volume{SCF}(round(float(v); digits=digits))
 
 # ========= CONSTANTS & CONVERSIONS ===========================================
 const FT3_PER_M3   = 35.3146667215
@@ -37,19 +37,19 @@ const FT3_PER_M3   = 35.3146667215
 # --- Conversion helpers ------------------------------------------------------
 
 "Convert to cubic meters"
-to_m3(v::Volume{M3})  = v
-to_m3(v::Volume{BBL}) = Volume(v.value * 159.0 / L_PER_M3, m3)
-to_m3(v::Volume{L})   = Volume(v.value / L_PER_M3, m3)
+to_m3(v::Volume{M3}; digits::Int=4)  = v
+to_m3(v::Volume{BBL}; digits::Int=4) = Volume(v.value * 159.0 / L_PER_M3, m3; digits=digits)
+to_m3(v::Volume{L}; digits::Int=4)   = Volume(v.value / L_PER_M3, m3; digits=digits)
 
 "Convert to barrels"
-to_bbl(v::Volume{BBL}) = v
-to_bbl(v::Volume{M3})  = Volume(v.value * L_PER_M3 / 159.0, bbl)
-to_bbl(v::Volume{L})   = Volume(v.value / 159.0, bbl)
+to_bbl(v::Volume{BBL}; digits::Int=4) = v
+to_bbl(v::Volume{M3}; digits::Int=4)  = Volume(v.value * L_PER_M3 / 159.0, bbl; digits=digits)
+to_bbl(v::Volume{L}; digits::Int=4)   = Volume(v.value / 159.0, bbl; digits=digits)
 
 "Convert to liters"
-to_ltr(v::Volume{L})   = v
-to_ltr(v::Volume{M3})  = Volume(v.value * L_PER_M3, ltr)
-to_ltr(v::Volume{BBL}) = Volume(v.value * 159.0, ltr)
+to_ltr(v::Volume{L}; digits::Int=4)   = v
+to_ltr(v::Volume{M3}; digits::Int=4)  = Volume(v.value * L_PER_M3, ltr; digits=digits)
+to_ltr(v::Volume{BBL}; digits::Int=4) = Volume(v.value * 159.0, ltr; digits=digits)
 
 # --- Pretty printing for Volume ----------------------------------------------
 Base.show(io::IO, v::Volume{M3})  = print(io, "$(v.value) m³")
